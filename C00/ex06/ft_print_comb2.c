3bc40877347de67b3e259ea78fd2e3d9dd739921
eepylaurie <eepylaurie@gmail.com>
2025-08-26 18:58:38 +0200

    ex06

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/25 21:58:31 by lmatthes          #+#    #+#             */
+/*   Updated: 2025/08/25 21:58:31 by lmatthes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			ft_putchar(i / 10 + '0');
+			ft_putchar(i % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(j / 10 + '0');
+			ft_putchar(j % 10 + '0');
+			if (!(i == 98 && j == 99))
+				write(1, ", ", 2);
+			j++;
+		}
+		i++;
+	}
+}
+
+/* int	main()
+{
+	ft_print_comb2();
+} */
